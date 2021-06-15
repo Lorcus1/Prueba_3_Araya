7fa2d7bee311898af979d48d39e72600c33e65da
Francisco <franci.arayag@duocuc.cl>
2021-06-15 17:56:18 -0400

    SignIn/SignUp

--- MasterRace/core/urls.py
+++ MasterRace/core/urls.py
@@ -1,11 +1,20 @@
 from core.models import Producto
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, agregarProducto,Producto
+from core.views import index, Producto, FormAddProd, Panel, SignIn, In, Out, SignUp, CreateUser
 
 urlpatterns = [
     path('',index, name='index'),
 
-    path('producto/',Producto, name='Producto'),
-    path('producto/agregarProducto',agregarProducto, name='agregarProducto'),
+    path('panel/', Panel, name='Panel'),
+    path('panel/formadd',FormAddProd, name='FormAddProd'),
+
+
+    path('signin/', SignIn, name='SignIn'),
+    path('signin/in/', In, name='In'),
+    path('signin/out/', Out, name='Out'),
+
+    path('signup/', SignUp, name='SignUp'),
+     path('signup/createuser', CreateUser, name='CreateUser'),
+
 ]
--- MasterRace/core/views.py
+++ MasterRace/core/views.py
@@ -1,17 +1,67 @@
 from core.models import Producto,Fabricante,Componente
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def index(request):
     productos = Producto.objects.all()
     contexto = {'productos': productos}
     return render(request, 'index.html',contexto)  
-    
 
-def agregarProducto(request):
+def FormAddProd(request):
     fabricantes = Fabricante.objects.all()
     componentes = Componente.objects.all()
     contexto = {'fabricantes': fabricantes, 'componentes': componentes}
-    ID = request.POST.get('ID','')
+    return render(request, 'agregarProducto.html',contexto)
 
-  
+#Administrativo 
+def Panel(request):
+    productos = Producto.objects.all()
+    contexto = {'productos': productos}
+    return render(request,'panel.html', contexto)
+
+    
+#Login
+def SignIn(request):
+    contexto={}
+    return render(request,'signin.html',contexto)
+
+def In(request):
+    username = request.POST.get('usuario','default')
+    password = request.POST.get('clave','default')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        
+        return redirect('index')
+    else:
+        return redirect(SignIn)
+
+def Out(request):
+    logout(request)
+    return redirect ('index')
+
+
+
+#Register
+
+def SignUp(request):
+    contexto={}
+    return render(request,'signup.html',contexto)
+
+def CreateUser(request):
+    contexto={}
+    username = request.POST.get('usuario','default')
+    email = request.POST.get('email','default')
+    password = request.POST.get('clave','default')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        return redirect('SignUp')
+    else:
+        user = User.objects.create_user(username, email, password)
+        return redirect('index')
+        user.save()
+    return render(request,'signin.html')
 
